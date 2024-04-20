1e72edc2b75d9ed8cf9dfe35199e8606e6abe666
Facundo Jimenez <facundo.gabriel.jimenez@gmail.com>
2024-04-19 23:27:56 -0300

    Agregado de funciones de lectura de sensores

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,5 +1,4 @@
 #include <Wire.h>
-
 #include <LiquidCrystal_I2C.h>
 
 // Crear el objeto lcd dirección 0x3F y 16 columnas x 2 Filas
@@ -38,6 +37,7 @@ const double CONST_CONV_CM = 0.01723;
 //{
   
 //}
+
 enum state_t { 
   STATE_WAITING_FOR_TRAINING, 
   STATE_READY_FOR_TRAINING, 
@@ -57,10 +57,30 @@ enum event_t {
   EVENT_CONTINUE,
   EVENT_MONITORING_TRAINING
 };
+
 event_t currentEvent; 
 state_t currentState;
 
- 
+
+int index = 0;
+const unsigned long MAX_SENSOR_LOOP_TIME = 50;
+const unsigned int NUMBER_OF_SENSORS = 4;
+unsigned long currentTime;
+unsigned long previousTime;
+
+String arrEstados[5] = { "STATE_WAITING_FOR_TRAINING", "STATE_READY_FOR_TRAINING", "STATE_TRAINING_IN_PROGRESS", "STATE_PAUSED_TRAINING", "STATE_TRAINING_FINISHED"};
+String arrEventos[9] = { "EVENT_TRAINING_RECEIVED", "EVENT_TRAINING_STARTED", "EVENT_TRAINING_PAUSED", "EVENT_TRAINING_RESUMED",
+"EVENT_TRAINING_CONCLUDED", "EVENT_TRAINING_CANCELLED", "EVENT_TRAINING_RESTARTED", "EVENT_CONTINUE", "EVENT_MONITORING_TRAINING"};
+
+void printEvento(int eventoIndex){
+  Serial.print("Evento actual: ");
+  Serial.println(arrEventos[eventoIndex]);
+}
+
+void printEstado(int estadoIndex){
+  Serial.print("Estado actual: ");
+  Serial.println(arrEstados[estadoIndex]);
+}
 
 void do_init() {
   // Acá puntualmente NO se inicializan los pines de los sensores de movimiento,
@@ -78,27 +98,65 @@ void do_init() {
 
     Serial.begin(SERIAL_SPEED);
 
-    // TOMA LA PRIMERA MEDICION DE TIEMPO
-    TIEMPO_ANTERIOR = millis();
 
     //Inicializo el primer estado
     currentState = STATE_WAITING_FOR_TRAINING;
+    currentEvent = EVENT_CONTINUE;
+
+    //Inicializa el tiempo
+    previousTime = millis();
 }
 
 
-void setup()
-{
-    do_init();
+//Funciones de atención a los sensores
+
+void checkPlayStopSensor(){
+  Serial.println("CHECK SENSOR PLAY/STOP");
 }
+
+void checkMediaControlSensor(){
+    Serial.println("CHECK SENSOR MEDIA CONTROL");
+
+}
+
+void checkSpeedSensor(){
+  Serial.println("CHECK SENSOR SPEED");
+}
+
+void checkTrainingButtonSensor(){
+  Serial.println("CHECK SENSOR TRAINING SENSOR");
+}
+
+void (*check_sensor[NUMBER_OF_SENSORS])() = { 
+  checkSpeedSensor, 
+  checkTrainingButtonSensor,
+  checkPlayStopSensor, 
+  checkMediaControlSensor 
+};
+
 
 void get_event(){
 
+    // verificar sensores
+    currentTime = millis();
+    if ((currentTime - previousTime) > MAX_SENSOR_LOOP_TIME)
+    {
+        check_sensor[index]();
+        index = ++index % NUMBER_OF_SENSORS;
+        previousTime = currentTime;
+    }
+    else
+    {
+        currentEvent = EVENT_CONTINUE;
+    }
 }
 
 
 
 void state_machine(){
   get_event();
+ printEvento(currentEvent);
+ printEstado(currentState);
 
   switch(currentState){
     case STATE_WAITING_FOR_TRAINING:
@@ -181,55 +239,65 @@ void state_machine(){
   }
 }
 
+void setup()
+{
+    do_init();
+}
+
 void loop()
 {
-    // TOMO EL TIEMPO ACTUAL
-    TIEMPO_ACTUAL = millis();
-    // Serial.println("Hola");
-    // VERIFICA CUANTO TRANSCURRIO DE TIEMPO
-
-    int valorPot = analogRead(HALL_SENSOR_PIN);
-
-    int velocidad_pedaleada = map(valorPot, 0, 1023, 0, 100);
-    Serial.println(velocidad_pedaleada);
-
-    if ((TIEMPO_ACTUAL - TIEMPO_ANTERIOR) >= velocidad_pedaleada && velocidad_pedaleada > 0)
-    {
-        TIEMPO_ANTERIOR = TIEMPO_ACTUAL;
-        lcd.setCursor(0, 0);
-        lcd.print("Vueltas");
-        lcd.setCursor(10, 0);
-        contador += 1;
-        lcd.print(contador);
-      	lcd.setCursor(0,1);
-        lcd.print("Velocidad");
-      	lcd.setCursor(10,1);
-      	lcd.print(velocidad_pedaleada);
-      	lcd.clear();
-    
-    }
-
-    if (velocidad_pedaleada <= VELOCIDAD_BAJA)
-    {
-        analogWrite(BLUE_LED_PIN, 255);
-        analogWrite(GREEN_LED_PIN, 0);
-        analogWrite(RED_LED_PIN, 0);
-    }
-    else if (velocidad_pedaleada > VELOCIDAD_BAJA && velocidad_pedaleada < VELOCIDAD_MEDIA)
-    {
-        analogWrite(BLUE_LED_PIN, 0);
-        analogWrite(GREEN_LED_PIN, 255);
-        analogWrite(RED_LED_PIN, 0);
-    }
-    else
-    {
-        analogWrite(BLUE_LED_PIN, 0);
-        analogWrite(GREEN_LED_PIN, 0);
-        analogWrite(RED_LED_PIN, 255);
-    }
-
-
+  state_machine();
 }
+
+// void loop()
+// {
+//     // TOMO EL TIEMPO ACTUAL
+//     TIEMPO_ACTUAL = millis();
+//     // Serial.println("Hola");
+//     // VERIFICA CUANTO TRANSCURRIO DE TIEMPO
+
+//     int valorPot = analogRead(HALL_SENSOR_PIN);
+
+//     int velocidad_pedaleada = map(valorPot, 0, 1023, 0, 100);
+//     Serial.println(velocidad_pedaleada);
+
+//     if ((TIEMPO_ACTUAL - TIEMPO_ANTERIOR) >= velocidad_pedaleada && velocidad_pedaleada > 0)
+//     {
+//         TIEMPO_ANTERIOR = TIEMPO_ACTUAL;
+//         lcd.setCursor(0, 0);
+//         lcd.print("Vueltas");
+//         lcd.setCursor(10, 0);
+//         contador += 1;
+//         lcd.print(contador);
+//       	lcd.setCursor(0,1);
+//         lcd.print("Velocidad");
+//       	lcd.setCursor(10,1);
+//       	lcd.print(velocidad_pedaleada);
+//       	lcd.clear();
+    
+//     }
+
+//     if (velocidad_pedaleada <= VELOCIDAD_BAJA)
+//     {
+//         analogWrite(BLUE_LED_PIN, 255);
+//         analogWrite(GREEN_LED_PIN, 0);
+//         analogWrite(RED_LED_PIN, 0);
+//     }
+//     else if (velocidad_pedaleada > VELOCIDAD_BAJA && velocidad_pedaleada < VELOCIDAD_MEDIA)
+//     {
+//         analogWrite(BLUE_LED_PIN, 0);
+//         analogWrite(GREEN_LED_PIN, 255);
+//         analogWrite(RED_LED_PIN, 0);
+//     }
+//     else
+//     {
+//         analogWrite(BLUE_LED_PIN, 0);
+//         analogWrite(GREEN_LED_PIN, 0);
+//         analogWrite(RED_LED_PIN, 255);
+//     }
+
+
+// }
 
 
 
